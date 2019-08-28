807f990a582614de00032c537f07969b56d6fafe
liontwinkle <lion.twinkle.ge@gmail.com>
2019-08-28 13:36:21 +0200

    remove the Any letter case for headers

--- src/utils/index.js
+++ src/utils/index.js
@@ -161,7 +161,7 @@ export const getObjectFromArray = (array) => {
 export const getProducts = (products) => {
   const keys = Object.keys(products[0]);
   const values = Object.values(products[0]);
-  const headers = keys.map(key => key.toLowerCase());
+  const headers = keys;
   const numbers = [];
   const columns = [];
   values.forEach((value, key) => {
