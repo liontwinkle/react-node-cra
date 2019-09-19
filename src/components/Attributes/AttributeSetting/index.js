894a7028bd3a6aaee429058b24fe7d5edcd1a4b3
liontwinkle <lion.twinkle.ge@gmail.com>
2019-09-19 21:56:06 +0200

    Building the category associations progressing3( added the checktree. )

--- src/components/Attributes/AttributeSetting/index.js
+++ src/components/Attributes/AttributeSetting/index.js
@@ -1,149 +1,69 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Tooltip } from 'react-tippy';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import CheckboxTree from 'react-checkbox-tree-enhanced';
 import { withSnackbar } from 'notistack';
 
-import AddIcon from '@material-ui/icons/Add';
-import DeleteIcon from '@material-ui/icons/Delete';
-
-
-import {
-  CustomSection,
-  CustomSelectWithLabel,
-  IconButton,
-} from 'components/elements';
 import { confirmMessage } from 'utils';
 
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import AttributeSettingAction from './AttributeSettingAction';
 import './style.scss';
 
-const checkNodes = [{
-  value: 'mars',
-  label: 'Mars',
-  id: 'ID',
-  children: [
-    {
-      value: 'phobos', label: 'Phobos', id: 'ID', children: [{ value: 'child', label: 'child', id: 'ID' }],
-    },
-    { value: 'deimos', label: 'Deimos', id: 'ID' },
-  ],
-}];
-
 class AttributeSetting extends Component {
   state = {
-    selectedGroup: null,
-    categoryList: [],
-    newCategory: null,
-    categories: [],
+    // selectedGroup: null,
+    // newCategory: null,
     checked: [],
     expanded: [],
   };
 
   componentDidMount() {
-    const { attribute, categories } = this.props;
-    this.updateState(categories, attribute.appear);
+    // const { attribute, categories } = this.props;
   }
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.attribute !== this.props.attribute) {
-      this.updateState(this.props.categories, this.props.attribute.appear);
-    }
+  componentDidUpdate() {
+    // if (prevProps.attribute !== this.props.attribute) {
+    // }
   }
 
-  updateState = (categories, list) => {
-    const newCategories = [];
-    const categoryList = [];
-    const categoriesData = categories.map((categoryItem => ({
-      label: categoryItem.name,
-      key: categoryItem._id,
-    })));
-
-    categoriesData.forEach((item) => {
-      if (list.filter(listItem => (listItem === item.key)).length === 0) {
-        newCategories.push(item);
-      } else {
-        categoryList.push(item);
-      }
-    });
-
-    this.setState({
-      categories: newCategories,
-      categoryList,
-    });
-  };
-
-  changeCategory = type => (value) => {
-    if (type === 'new') {
-      this.setState({
-        newCategory: value,
-      });
-    } else if (this.checkGorupPermission(type)) {
-      this.setState((prevState) => {
-        const currentState = prevState;
-        const tempCategories = [...currentState.categories, currentState.categoryList[type]];
-        const changeCategories = tempCategories.filter(item => (item !== value));
-        currentState.categoryList[type] = value;
-        currentState.categories = changeCategories;
-
-        return {
-          ...currentState,
-        };
-      });
-    } else {
-      confirmMessage(
-        this.props.enqueueSnackbar,
-        'This attribute could not be changed since the group is selected.',
-        'info',
-      );
-    }
-  };
-
-  checkGorupPermission= (type) => {
+  checkGroupPermission= (value) => {
     const group = this.props.nodes.filter(nodeItem => (nodeItem.item._id === this.props.attribute.groupId));
     let updateFlag = true;
     if (group.length > 0) {
-      updateFlag = !(group[0].item.appear.find(arrItem => (arrItem === this.state.categoryList[type].key)));
+      updateFlag = !(group[0].item.appear.find(arrItem => (arrItem === value)));
     }
     return updateFlag;
   };
 
-  addCategory = () => {
-    if (this.state.newCategory) {
-      this.setState(prevState => (
-        {
-          categoryList: [...prevState.categoryList, prevState.newCategory],
-          categories: prevState.categories.filter(item => (item !== prevState.newCategory)),
-          newCategory: null,
-        }
-      ));
+
+  handleCheck = (checked) => {
+    console.log('checked>>>', checked);
+    const stateLen = this.state.checked.length;
+    const updateLen = checked.length;
+    let value = '';
+    if (stateLen > updateLen) {
+      value = this.state.checked[stateLen - 1];
     } else {
-      confirmMessage(this.props.enqueueSnackbar, 'There is no any updated categories.', 'info');
+      value = checked[updateLen - 1];
+    }
+    if (this.checkGroupPermission(value)) {
+      console.log('# DEBUG: UPDATE'); // fixme
+      this.setState({ checked });
+    } else {
+      confirmMessage(this.props.enqueueSnackbar, 'This attribute is changeable on the group only', 'info');
     }
   };
 
-  deleteCategory = (key) => {
-    if (this.checkGorupPermission(key)) {
-      this.setState(prevState => (
-        {
-          categoryList: prevState.categoryList.filter((item, keyItem) => (keyItem !== key)),
-          categories: [...prevState.categories, prevState.categoryList[key]],
-          newCategory: null,
-        }
-      ));
-    } else {
-      confirmMessage(this.props.enqueueSnackbar, 'There is no any updated categories.', 'info');
-    }
+  handleExpand = (expanded) => {
+    console.log('expand>>>>>', expanded);
+    this.setState({ expanded });
   };
 
   render() {
     const {
       categoryList,
-      newCategory,
-      categories,
     } = this.state;
 
     const {
@@ -151,8 +71,6 @@ class AttributeSetting extends Component {
       assoicationCategories,
     } = this.props;
 
-    console.log('org trees>>>', checkNodes); // fixme
-    console.log('trees>>>', assoicationCategories); // fixme
     return (
       <div className="mg-attr-setting-container d-flex">
         <PerfectScrollbar
@@ -165,52 +83,15 @@ class AttributeSetting extends Component {
             nodes={assoicationCategories}
             checked={this.state.checked}
             expanded={this.state.expanded}
-            onCheck={checked => this.setState({ checked })}
-            onExpand={expanded => this.setState({ expanded })}
+            onCheck={this.handleCheck}
+            onExpand={this.handleExpand}
+            nativeCheckboxes
+            showNodeIcon={false}
+            icons={{
+              expandClose: <FontAwesomeIcon className="rct-icon rct-icon-expand-close" icon="chevron-right" />,
+              expandOpen: <FontAwesomeIcon className="rct-icon rct-icon-expand-open" icon="chevron-down" />,
+            }}
           />
-          <CustomSection title="Associated Category" key="associated_category">
-            {
-              categoryList.map((listItem, key) => (
-                <div className="mg-setting-section" key={parseInt(key, 10)}>
-                  <CustomSelectWithLabel
-                    label={`Category ${parseInt([key], 10) + 1}`}
-                    inline
-                    value={categoryList[key]}
-                    items={categories || []}
-                    onChange={this.changeCategory(key)}
-                  />
-                  <Tooltip
-                    title="Delete Category"
-                    position="bottom"
-                    arrow
-                  >
-                    <IconButton onClick={() => this.deleteCategory(parseInt(key, 10))}>
-                      <DeleteIcon style={{ fontSize: 20 }} />
-                    </IconButton>
-                  </Tooltip>
-                </div>
-              ))
-            }
-            <div className="mg-setting-section">
-              <CustomSelectWithLabel
-                label="Category New"
-                inline
-                value={newCategory}
-                items={categories || []}
-                onChange={this.changeCategory('new')}
-                key
-              />
-              <Tooltip
-                title="Add New Category Association"
-                position="bottom"
-                arrow
-              >
-                <IconButton onClick={this.addCategory}>
-                  <AddIcon style={{ fontSize: 20 }} />
-                </IconButton>
-              </Tooltip>
-            </div>
-          </CustomSection>
         </PerfectScrollbar>
         <AttributeSettingAction attribute={attribute} categoryList={categoryList} />
       </div>
@@ -221,7 +102,6 @@ class AttributeSetting extends Component {
 AttributeSetting.propTypes = {
   enqueueSnackbar: PropTypes.func.isRequired,
   attribute: PropTypes.object.isRequired,
-  categories: PropTypes.array.isRequired,
   nodes: PropTypes.array.isRequired,
   assoicationCategories: PropTypes.array.isRequired,
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -131,7 +131,7 @@ const getSubTree = (list, parentId, type) => {
     sublist.forEach((item) => {
       association.push({
         label: item.name,
-        value: item._id,
+        value: item.name, // fixme
         children: getSubTree(list, item._id, type).association,
       });
       subTree.push({
