c12db2032281339e08591ae1a7eefb4e76e75ce4
liontwinkle <lion.twinkle.ge@gmail.com>
2019-09-28 15:45:25 +0200

    reduce the loading time_progressing2

--- src/components/ProductTable/index.js
+++ src/components/ProductTable/index.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import $ from 'jquery';
 import PropTypes from 'prop-types';
-import PerfectScrollbar from 'react-perfect-scrollbar';
+// import PerfectScrollbar from 'react-perfect-scrollbar';
 import { withSnackbar } from 'notistack';
 import { HotTable } from '@handsontable/react';
 import _isEqual from 'lodash/isEqual';
@@ -40,10 +40,17 @@ class ProductTable extends Component {
     }
   }
 
+  // UNSAFE_componentWillUpdate(nextProps) {
+  //   if (nextProps.isUpdating !== this.props.isUpdating) {
+  //     console.log('#### DEBUG FLAG IS WillUPDATED#####', performance.now()); // fixme
+  //     this.setFetchFg(nextProps.isUpdating);
+  //   }
+  // }
+
   componentDidUpdate(prevProps) {
     if ((prevProps.isUpdating !== this.props.isUpdating) && this.props.isUpdating) {
       console.log('#### DEBUG FLAG IS UPDATED#####', performance.now()); // fixme
-      this.setFetchFg(this.props.isUpdating);
+      // this.setFetchFg(this.props.isUpdating);
     }
 
     if ((prevProps.isUpdatingList !== this.props.isUpdatingList) && this.props.isUpdatingList) {
@@ -110,48 +117,47 @@ class ProductTable extends Component {
       tableRef,
     } = this.props;
 
+    // console.log('')
     return (
       <div id="hot-app">
         {(!this.state.fetchingFlag)
           ? (
-            <PerfectScrollbar>
-              <HotTable
-                ref={tableRef}
-                root="hot"
-                licenseKey="non-commercial-and-evaluation"
-                afterChange={this.setChangeItem}
-                afterFilter={this.makeFilterResult}
-                settings={{
-                  data: products,
-                  columns,
-                  width: '100%',
-                  height: '100%',
-                  headerTooltips: true,
-                  colHeaders: headers,
-                  rowHeaders: true,
-                  contextMenu: [
-                    'copy',
-                    'cut',
-                    'hidden_columns_hide',
-                    'hidden_columns_show',
-                    'make_read_only',
-                  ],
-                  viewportRowRenderingOffset: 70,
-                  viewportColumnRenderingOffset: 10,
-                  filters: true,
-                  dropdownMenu: ['filter_by_condition', 'filter_action_bar'],
-                  manualColumnResize: true,
-                  manualRowResize: true,
-                  manualColumnMove: true,
-                  manualRowMove: true,
-                  // multiColumnSorting: true,
-                  hiddenColumns: {
-                    columns: this.state.hiddenColumns,
-                    indicators: true,
-                  },
-                }}
-              />
-            </PerfectScrollbar>
+            <HotTable
+              ref={tableRef}
+              root="hot"
+              licenseKey="non-commercial-and-evaluation"
+              afterChange={this.setChangeItem}
+              afterFilter={this.makeFilterResult}
+              settings={{
+                data: products,
+                columns,
+                width: '100%',
+                height: '100%',
+                headerTooltips: true,
+                colHeaders: headers,
+                rowHeaders: true,
+                contextMenu: [
+                  'copy',
+                  'cut',
+                  'hidden_columns_hide',
+                  'hidden_columns_show',
+                  'make_read_only',
+                ],
+                viewportRowRenderingOffset: 70,
+                viewportColumnRenderingOffset: 10,
+                filters: true,
+                dropdownMenu: ['filter_by_condition', 'filter_action_bar'],
+                manualColumnResize: true,
+                manualRowResize: true,
+                manualColumnMove: true,
+                manualRowMove: true,
+                // multiColumnSorting: true,
+                hiddenColumns: {
+                  columns: this.state.hiddenColumns,
+                  indicators: true,
+                },
+              }}
+            />
           ) : (
             <div className="table-loader">
               <Loader size="small" color="dark" />
