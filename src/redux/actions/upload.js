ba6dcf438b125155654243e100f1d0ccc3ca2165
liontwinkle <lion.twinkle.ge@gmail.com>
2019-10-02 21:55:20 +0200

    Implementing the upload data UI_progressing5

--- src/redux/actions/upload.js
+++ src/redux/actions/upload.js
@@ -1,20 +1,42 @@
 // import uploadService from 'services/upload.service';
 // import types from '../actionTypes';
 
+const validateKey = {
+  virtual: ['parentId', 'name'],
+  attributes: ['groupId', 'name', 'appear'],
+  native: ['parentId', 'name'],
+};
+
 const validateVirtualData = (data) => {
   console.log('### DEBUG VALIDATE VIRTUAL DATA', data);
+  const result = true;
+  const validateData = [];
+  data.forEach((dataItem) => {
+    console.log('### DEBUG SUB ITEM: ', dataItem); // fixme
+    const keys = Object.keys(dataItem);
+    console.log('### DEBUG SUB KEYS: ', keys); // fixme
+    if (keys.length > 0) {
+      let validateFlag = true;
+      validateKey.virtual.forEach((validateItem) => {
+        if (keys.findIndex(validateItem) === -1) { validateFlag = false; }
+      });
+      if (validateFlag) { validateData.push(dataItem); }
+    }
+  });
+  console.log('### DEBUG VALIDATE DATA: ', validateData); // fixme
+  return result;
 };
 
 const validateAttributeData = (data) => {
-  console.log('### DEBUG VALIDATE VIRTUAL DATA', data);
+  console.log('### DEBUG VALIDATE ATTRIBUTE DATA', data);
 };
 
 const validateNativeData = (data) => {
-  console.log('### DEBUG VALIDATE VIRTUAL DATA', data);
+  console.log('### DEBUG VALIDATE NATIVE DATA', data);
 };
 
 const validateProductsData = (data) => {
-  console.log('### DEBUG VALIDATE VIRTUAL DATA', data);
+  console.log('### DEBUG VALIDATE PRODUCTS DATA', data);
 };
 
 const validateData = (type, data) => {
