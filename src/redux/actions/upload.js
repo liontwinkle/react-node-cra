137dcb74a714d68311f43292a06c62c46a7c23b2
liontwinkle <lion.twinkle.ge@gmail.com>
2019-10-02 20:39:51 +0200

    Implementing the upload data UI_progressing4

--- src/redux/actions/upload.js
+++ src/redux/actions/upload.js
@@ -1,6 +1,37 @@
 // import uploadService from 'services/upload.service';
-import types from '../actionTypes';
+// import types from '../actionTypes';
 
+const validateVirtualData = (data) => {
+  console.log('### DEBUG VALIDATE VIRTUAL DATA', data);
+};
+
+const validateAttributeData = (data) => {
+  console.log('### DEBUG VALIDATE VIRTUAL DATA', data);
+};
+
+const validateNativeData = (data) => {
+  console.log('### DEBUG VALIDATE VIRTUAL DATA', data);
+};
+
+const validateProductsData = (data) => {
+  console.log('### DEBUG VALIDATE VIRTUAL DATA', data);
+};
+
+const validateData = (type, data) => {
+  console.log('### DEBUG TYPE: ', type);
+  switch (type) {
+    case 'virtual':
+      return validateVirtualData(data);
+    case 'attributes':
+      return validateAttributeData(data);
+    case 'products':
+      return validateProductsData(data);
+    case 'native':
+      return validateNativeData(data);
+    default:
+      return false;
+  }
+};
 const fileUpload = data => (dispatch, getState) => {
   if (getState().uploadData.isUploading) {
     return Promise.reject();
@@ -12,9 +43,16 @@ const fileUpload = data => (dispatch, getState) => {
   console.log('### DEBUG DATA: ', data); // fixme
   console.log('### DEBUG CLIENT: ', client.id); // fixme
   console.log('### DEBUG TYPE: ', type.key); // fixme
-  dispatch({
-    type: types.UPLOAD_DATA_REQUEST,
-  });
+
+  if (validateData(type.key, data)) {
+    console.log('##### DEBUG PASSED VALIDATE #####'); // fixme
+  } else {
+    console.log('##### DEBUG NOT PASSED VALIDATE #####'); // fixme
+  }
+
+  // dispatch({
+  //   type: types.UPLOAD_DATA_REQUEST,
+  // });
 
   // return uploadService.upload(client.id, type.key, data)
   //   .then((data) => {
