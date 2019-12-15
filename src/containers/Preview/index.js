fe00b8320a449c3ad35f51ba1efaaba38f912920
liontwinkle <lion.twinkle.ge@gmail.com>
2019-12-15 23:15:28 +0100

    Add loading bar when read proudcts

--- src/containers/Preview/index.js
+++ src/containers/Preview/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Redirect } from 'react-router-dom';
+
 import { setPreviewCategory, movePointedPath } from 'redux/actions/preview';
 import TopCategory from '../../components/Preview/TopCategory';
 import LeftNavigation from '../../components/Preview/LeftNavigation';
@@ -15,19 +17,23 @@ class Preview extends Component {
     super(props);
     this.state = {
       subCategories: [],
+      lastChildNode: false,
     };
   }
 
   componentDidMount() {
-
   }
+
 
   componentDidUpdate(prevProps) {
     const { selectedCategory, categories } = this.props;
     if (selectedCategory._id && selectedCategory !== prevProps.selectedCategory) {
       const subCategories = categories.filter((categoryItem) => (categoryItem.parent_id === selectedCategory._id));
+      let lastChildNode = false;
+      if (subCategories.length === 0) { lastChildNode = true; }
       this.updateState({
         subCategories,
+        lastChildNode,
       });
     }
   }
@@ -48,32 +54,40 @@ class Preview extends Component {
 
   render() {
     const { urlPath } = this.props;
-    const { subCategories } = this.state;
+    const { subCategories, lastChildNode } = this.state;
     return (
       <div className="preview_container">
-        <div className="preview_top">
-          <TopCategory />
-          <span className="preview_route">
-            <ul className="url_path">
-              {
-                urlPath.map((item) => (
-                  <li key={item.id} onClick={this.handleTopURLClick(item.id)}>{`${item.name} >`}</li>
-                ))
-              }
-            </ul>
-          </span>
-        </div>
-        <div className="preview_main">
-          <CategoryView subCategories={subCategories} />
-          {
-            subCategories.length === 0 && (
-              <>
-                <LeftNavigation />
-                <ProductBody />
-              </>
-            )
-          }
-        </div>
+        {
+          !this.props.client ? (
+            <Redirect to="/" />
+          ) : (
+            <>
+              <div className="preview_top">
+                <TopCategory />
+                <span className="preview_route">
+                  <ul className="url_path">
+                    {
+                      urlPath.map((item) => (
+                        <li key={item.id} onClick={this.handleTopURLClick(item.id)}>{`${item.name} >`}</li>
+                      ))
+                    }
+                  </ul>
+                </span>
+              </div>
+              <div className="preview_main">
+                <CategoryView subCategories={subCategories} />
+                {
+                  lastChildNode && subCategories.length === 0 && (
+                    <>
+                      <LeftNavigation />
+                      <ProductBody />
+                    </>
+                  )
+                }
+              </div>
+            </>
+          )
+        }
       </div>
     );
   }
@@ -83,6 +97,7 @@ Preview.propTypes = {
   urlPath: PropTypes.array.isRequired,
   categories: PropTypes.array.isRequired,
   selectedCategory: PropTypes.object.isRequired,
+  client: PropTypes.object.isRequired,
   setPreviewCategory: PropTypes.func.isRequired,
   movePointedPath: PropTypes.func.isRequired,
 };
@@ -91,6 +106,7 @@ const mapStateToProps = (store) => ({
   urlPath: store.previewData.urlPath,
   selectedCategory: store.previewData.selectedCategory,
   categories: store.categoriesData.categories,
+  client: store.clientsData.client,
 });
 
 const mapDispatchToProps = (dispatch) => bindActionCreators({
